c84b33bb545377e4df8d3e76f3ea3561c60e49e0
lck898989 <1783887127@qq.com>
2020-02-08 19:43:29 +0800

    add 3d game onetwosteps

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ app.use("/game",express.static(path.join(__dirname,"game")));
 app.use("/egret-game",express.static(path.join(__dirname,"egret-game")));
 app.use("/linegame",express.static(path.join(__dirname,"linegame")));
 app.use("/remote-assets",express.static(path.join(__dirname,"upload/remote-assets")));
+app.use("/onetwosteps/3d/",express.static(path.join(__dirname,"oneTwoStep3D")));
+
 app.all("*",(req,res,next) => {
   res.header("Access-Control-Allow-Origin","*");
   res.header("Access-Control-Allow-Headers","content-type");
